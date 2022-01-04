8cd2992c2cfc6b152bf782270ccbd13f5b649c6d
Paul <paul.wroe@act-on.com>
2022-01-03 21:26:39 -0800

    added entry point for express

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('images'))
+app.use(express.static(__dirname))
+
+app.get('/', (req, res) => {
+  // res.send('Hello Bill!')
+  // pre-process weather.js here, then serve the result
+  // or specify a route like '/weather.js' and use a callback script from the web page
+  res.sendFile("index.html");
+})
+
+app.listen(port, () => {
+  console.log(`Tunneling Pi app listening at http://localhost:${port}`)
+})
