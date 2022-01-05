c20b4450042223a4ddfaa471cb2b8ebe754e50c1
Kris Hatch-McCarter <userkris169@yahoo.com>
2022-01-04 19:31:46 -0800

    testing hello route

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const port = 3000;
 app.use(express.static("images"));
 app.use(express.static(__dirname));
 
-app.get("/", (req, res) => {
-	// pre-process weather.js here, then serve the result
-	// or specify a route like '/weather.js' and use a callback script from the web page
-	res.sendFile("index.html");
-});
+// app.get("/", (req, res) => {
+// 	// pre-process weather.js here, then serve the result
+// 	// or specify a route like '/weather.js' and use a callback script from the web page
+// 	res.sendFile("index.html");
+// });
 
 app.get("/hello", (req, res) => {
-	res.send("<h1>I Love You, You little developer you</h1>");
+	res.send("<h1>I Love You, You developer you</h1>");
 });
 
 app.listen(port, () => {
