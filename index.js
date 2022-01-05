f0a9e7184d9f1db4c8fce41566fbd1cd699fee11
Kris Hatch-McCarter <userkris169@yahoo.com>
2022-01-04 19:23:35 -0800

    try different route

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const port = 3000;
 app.use(express.static("images"));
 app.use(express.static(__dirname));
 
-app.get("/beans", (req, res) => {
-	res.send("Hello Bill!");
-});
-
 app.get("/", (req, res) => {
 	// pre-process weather.js here, then serve the result
 	// or specify a route like '/weather.js' and use a callback script from the web page
 	res.sendFile("index.html");
+});
+
+app.get("/beans", (req, res) => {
+	res.send("Hello Bill!");
 });
 
 app.listen(port, () => {
