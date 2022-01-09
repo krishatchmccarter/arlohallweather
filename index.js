af560f1fb3a4260a2ae44c66ccbf24d96492dd60
Kris Hatch-McCarter <userkris169@yahoo.com>
2022-01-09 12:27:23 -0800

    commit

--- index.js
+++ index.js
@@ -5,11 +5,21 @@ const port = 3000;
 app.use(express.static("images"));
 app.use(express.static(__dirname));
 
-// app.get("/", (req, res) => {
-// 	// pre-process weather.js here, then serve the result
-// 	// or specify a route like '/weather.js' and use a callback script from the web page
-// 	res.sendFile("index.html");
-// });
+app.get("/", (req, res) => {
+	const WeatherUndergroundNode = require("weather-underground-node");
+	require("dotenv").config();
+	const myApyKey = process.env.API_KEY;
+	const wunderground = new WeatherUndergroundNode(myApyKey);
+
+	wunderground
+		.PWSCurrentConditions("KORPORTL1801")
+		.InImperialUnits()
+		.request(function (err, response) {
+			const weatherData = response.observations[0];
+			console.log(weatherData);
+			res.send(weatherData);
+		});
+});
 
 app.get("/hello", (req, res) => {
 	res.send("<h1>I Love You, You developer you</h1>");
